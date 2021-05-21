659ad96d63c771db4acc3f1bbd79a6c46821700d
Don <angheloko@gmail.com>
2021-05-21 11:22:12 +0800

    Initial commit

---
+++ components/Gallery.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    class="flex-grow flex items-start overflow-x-hidden"
+    @mousedown="mouseDownHandler"
+    @mouseup="mouseUpHandler"
+    @mousemove="mouseMoveHandler"
+  >
+    <div v-for="i in 10" :key="i" class="pl-8 last:pr-8">
+      <div class="h-40 w-40 flex-none shadow bg-blue-600">
+        {{ i }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Gallery',
+  data () {
+    return {
+      isDragging: false,
+      left: 0,
+      x: 0
+    }
+  },
+  methods: {
+    mouseDownHandler (e) {
+      this.isDragging = true
+      this.x = e.clientX
+      this.left = this.$el.scrollLeft
+    },
+    mouseUpHandler () {
+      this.isDragging = false
+    },
+    mouseMoveHandler (e) {
+      if (this.isDragging) {
+        const distance = e.clientX - this.x
+        this.$el.scrollLeft = this.left - distance
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="h-full py-4 flex flex-col">
+    <div class="h-16 mx-8 mb-8 flex justify-between items-center">
+      <div class="p-1 bg-white shadow rounded-full">
+        <img src="/avatar.jpg" class="w-14 h-14 object-cover object-center rounded-full" />
+      </div>
+      <div>
+        <LightMode class="h-8 w-8" />
+      </div>
+    </div>
+    <div>
+      <Gallery />
+      <div class="mt-8 flex justify-center items-center">
+        <div class="flex text-gray-600">
+          <div class="p-2 mx-2 bg-white rounded-full shadow">
+            <Blog class="w-14 h-14 fill-current" />
+          </div>
+          <div class="p-2 mx-2 bg-white rounded-full shadow">
+            <GitHub class="w-14 h-14 fill-current" />
+          </div>
+          <div class="p-2 mx-2 bg-white rounded-full shadow">
+            <LinkedIn class="w-14 h-14 fill-current" />
+          </div>
+          <div class="p-2 mx-2 bg-white rounded-full shadow">
+            <Twitter class="w-14 h-14 fill-current" />
+          </div>
+          <div class="p-2 mx-2 bg-white rounded-full shadow">
+            <Mail class="w-14 h-14 fill-current" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="h-16 mx-8 mt-8 py-4 border-t border-gray-800">
+      &nbsp;Some text.
+    </div>
+  </div>
+</template>
+
+<script>
+import LightMode from '../components/icons/LightMode'
+import GitHub from '../components/icons/GitHub'
+import LinkedIn from '../components/icons/LinkedIn'
+import Twitter from '../components/icons/Twitter'
+import Mail from '../components/icons/Mail'
+import Blog from '../components/icons/Blog'
+import Gallery from '../components/Gallery'
+export default {
+  components: { Gallery, Blog, Mail, Twitter, LinkedIn, GitHub, LightMode }
+}
+</script>
+
+<style>
+</style>
