ae099cef9d6a190f3a84b8c3344498f0b73d913b
Don <angheloko@gmail.com>
2021-05-22 13:29:38 +0800

    - Updated site name and description

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,14 +8,14 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'switch-portfolio',
+    title: 'Switch UI Portfolio by Don Lalicon',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'A portfolio / resume / CV inspired by the Nintendo Switch UI' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
